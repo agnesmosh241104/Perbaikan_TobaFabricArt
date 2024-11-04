195d3fd3490b2dbe848ae08827480f5f9fe643ac
rosarisimanjuntak16 <112604185+rosarisimanjuntak16@users.noreply.github.com>
2024-11-05 02:45:46 +0700

    mencoba menyelesaikan fitur shop dan contact us

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,6 +21,9 @@
 <body>
 
 	<!-- Start Header/Navigation -->
+
+
+
 	<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
 		<div class="container">
