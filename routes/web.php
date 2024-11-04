a8721b076cdad9bf5f877b101ee20485ed2330f6
difyaambarita <112596574+difyaambarita@users.noreply.github.com>
2024-11-05 02:49:21 +0700

    About

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,53 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', function () {
+    return view('welcome'); // Change 'welcome' if you have a different homepage file
+});
+
+// Route to the "Thank You" page
+Route::get('/thankyou', function () {
+    return view('thankyou');
+});
+
+// Route to the "Shop" page
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+// Route to the "Contact" page
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+// Route to the "Contact" page
+Route::get('/service', function () {
+    return view('service');
+});
+
+
+// Route to the "Checkout" page (make sure the filename matches exactly)
+Route::get('/checkout', function () {
+    return view('chekout');
+});
+
+// Route to the "Cart" page
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+// Route to the "Blog" page
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+// Route to the "About" page
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/cart', function () {
+    return view('cart'); // 'cart' refers to 'cart.blade.php' in the views folder
+})->name('cart');
